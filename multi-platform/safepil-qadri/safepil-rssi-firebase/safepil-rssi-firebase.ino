f2309580eec4d64902cbee5d876081aa3dee1f16
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-04-30 18:33:33 +0700

    safepil rssi wemos to firebase

---
+++ multi-platform/safepil-qadri/safepil-rssi-firebase/safepil-rssi-firebase.ino
@@ -0,0 +1,94 @@
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "PLAZGOZZ_OUT"
+#define WIFI_PASSWORD "grapari78"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyBc6X4IjEKCtd1pw_2Cc6DIkrmXbqT3Hmg"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://safepil-default-rtdb.asia-southeast1.firebasedatabase.app" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+long intervalMillis = 5000;
+int count = 0;
+bool signupOK = false;
+
+String deviceId = "device_11";
+String fbDir = "main/realtime/" + deviceId;
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+  long rssiVal = WiFi.RSSI();
+
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > intervalMillis || sendDataPrevMillis == 0)){
+    Serial.print("RSSI  : ");
+    Serial.println(rssiVal);
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, fbDir + "/rssi", rssiVal)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    
+        
+  }
+}
