b1a74e75c55256bef5f41bf50bb5be4a12d2a0f4
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-05-18 07:39:37 +0700

    connect to esp32 cam using bluetooth

--- arduino-ide-based/pir-cam-supriyadi-040424/pirToCam/pirToCam.ino
+++ arduino-ide-based/pir-cam-supriyadi-040424/pirToCam/pirToCam.ino
@@ -1,26 +1,11 @@
-
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
-
+#include <BluetoothSerial.h>
 
 #define pirPin 13
 #define ledPin 2
-#define outputCamPin 15
+#define relayPin 5
 
+BluetoothSerial CameraBT;
 
-// Wifi network station credentials
-#define WIFI_SSID "MIKRO"
-#define WIFI_PASSWORD "IDEAlist"
-// Telegram BOT Token (Get from Botfather)
-#define BOT_TOKEN "1115765927:AAFgDI003Xn41tererJRuoU543tBsg8CBpE"
-
-const unsigned long BOT_MTBS = 1000;  // mean time between scan messages
-String CHAT_ID = "108488036";
-
-WiFiClientSecure secured_client;
-UniversalTelegramBot bot(BOT_TOKEN, secured_client);
-unsigned long bot_lasttime;  // last time messages' scan has been done
 
 int adaGerak = 0;
 int lastGerak = 0;
@@ -28,70 +13,58 @@ int lastGerak = 0;
 void setup() {
   Serial.begin(115200);  // Open serial monitor at 115200 baud to see ping results.
 
-  pinMode(outputCamPin, OUTPUT);
-  digitalWrite(outputCamPin, HIGH);  //HIGH = camera standby
-
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
+  delay(1000);
+  digitalWrite(relayPin, LOW);
+
   pinMode(pirPin, INPUT);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);  // Add root certificate for api.telegram.org
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.print("\nWiFi connected. IP address: ");
-  Serial.println(WiFi.localIP());
+  CameraBT.begin();  
+  Serial.println("Bluetooth Started! Ready to pair...");
 
-  Serial.print("Retrieving time: ");
-  configTime(0, 0, "pool.ntp.org");  // get UTC time via NTP
-  time_t now = time(nullptr);
-  while (now < 24 * 3600) {
-    Serial.print(".");
-    delay(100);
-    now = time(nullptr);
-  }
-  Serial.println(now);
+  kedipLed(2);
 }
 
 void loop() {
 
-  if (millis() - bot_lasttime > BOT_MTBS) {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-    while (numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  if (CameraBT.available() > 0) {
+    String dataCamera = CameraBT.readStringUntil('\n');
+    dataCamera.trim();
+    Serial.print("Dari Camera : ");
+    Serial.println(dataCamera);
+    kedipLed(0.5);
+    if(dataCamera == "Buka Pintu"){
+      digitalWrite(relayPin, HIGH);
+    }else if(dataCamera == "Kunci Pintu"){
+      digitalWrite(relayPin, LOW);
     }
-
-    adaGerak = digitalRead(pirPin);
-    // Serial.print("ADA GERAK : ");
-    // Serial.print(adaGerak);
-    // Serial.println();
-
-    if (adaGerak != lastGerak) {  //filter untuk mendeteksi perubahan status
-      if (adaGerak) {
-        Serial.println("Gerakan Terdeteksi");
-        digitalWrite(outputCamPin, LOW);
-        digitalWrite(ledPin, HIGH);
-        delay(10);
-        digitalWrite(outputCamPin, HIGH);
-        digitalWrite(ledPin, LOW);
-      }
-    }
-
-    lastGerak = adaGerak;
-
-    bot_lasttime = millis();
+    
   }
+
+  adaGerak = digitalRead(pirPin);
+  // Serial.print("ADA GERAK : ");
+  // Serial.print(adaGerak);
+  // Serial.println();
+
+  if (adaGerak != lastGerak) {  //filter untuk mendeteksi perubahan status
+    if (adaGerak) {
+      Serial.println("Gerakan Terdeteksi");      
+      CameraBT.print("Gerakan Terdeteksi");
+      CameraBT.print("\n");
+      kedipLed(3);    
+    }
+  }
+
+  lastGerak = adaGerak;
+  delay(50);
 }
 
-
-void handleNewMessages(int numNewMessages) {
-  for (int i = 0; i < numNewMessages; i++) {
-    bot.sendMessage(bot.messages[i].chat_id, bot.messages[i].text, "");
-  }
+void kedipLed(float detik) {
+  digitalWrite(ledPin, HIGH);
+  delay(detik * 1000);
+  digitalWrite(ledPin, LOW);
 }
