23c791cecdcd5cc0360f94497f6bbb9304d4f9c7
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-06-04 02:50:47 +0700

    add ultrasonic and offline mode

--- arduino-ide-based/portal_parkir_blynk/portal_parkir_blynk.ino
+++ arduino-ide-based/portal_parkir_blynk/portal_parkir_blynk.ino
@@ -15,6 +15,9 @@
 #define infra2Pin 19
 #define ldrPin 5
 #define servoPin 23
+#define ledPin 2
+#define buzzerPin 15
+
 
 #define trig1Pin 32
 #define trig2Pin 25
@@ -28,8 +31,8 @@
 DimmableLight dimmer1(dimmer1Pin);
 DimmableLight dimmer2(dimmer2Pin);
 
-char ssid[] = "MIKRO";
-char pass[] = "IDEAlist";
+char ssid[] = "Abuuuu";
+char pass[] = "555111333";
 
 BlynkTimer timer;
 
@@ -42,6 +45,8 @@ bool nowSiang = false;
 
 const int maxJarak = 200;
 const int batasJarak = 10;
+const int batasBunyi = 8;
+
 
 Servo myservo = Servo();
 NewPing sonar1(trig1Pin, echo1Pin, maxJarak);
@@ -57,14 +62,49 @@ int infra1Val, infra2Val, ldrVal;
 int lastInfra1, lastInfra2;
 int ultra1Val, ultra2Val;
 
+int limitCariWifi = 10;
+int cariWifiCount = 0;
 
+bool modeOffline = false;
 void setup() {
-  Serial.begin(115200);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW);
 
   pinMode(infra1Pin, INPUT);
   pinMode(infra2Pin, INPUT);
 
+  Serial.begin(115200);
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+
+  Serial.println("Connecting to WiFi..");
+  while (WiFi.status() != WL_CONNECTED) {
+    if (cariWifiCount >= limitCariWifi) {
+      break;
+    }
+    delay(1000);
+    kedipLed(0.2);
+    Serial.print(".");
+    cariWifiCount++;
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    modeOffline = true;
+    Serial.println("OFFLINE");
+    kedipLed(3);
+
+
+  } else {
+    Serial.println(WiFi.localIP());
+    Blynk.config(BLYNK_AUTH_TOKEN);
+  }
+
+
+  // Print ESP32 Local IP Address
   myservo.write(servoPin, sudutTutup);
 
   infra1Val = !digitalRead(infra1Pin);
@@ -74,6 +114,9 @@ void setup() {
 
   DimmableLight::setSyncPin(zeroCrossPin);
   DimmableLight::begin();
+
+  kedipLed(1);
+
 
   timer.setInterval(500L, myTimerEvent);
 }
@@ -90,19 +133,36 @@ void myTimerEvent() {
 
   debugSensor();
 
-  Blynk.virtualWrite(V1, ultra1Val);
-  Blynk.virtualWrite(V2, ultra2Val);
+  if (modeOffline == false) {
+    Blynk.virtualWrite(V1, ultra1Val);
+    Blynk.virtualWrite(V2, ultra2Val);
+  }
 
   nowSiang = ldrVal;
 
   if (ultra1Val < batasJarak) {
+
     if (nowSiang) {
       dimmer1.setBrightness(terangSiang);
     } else {
       dimmer1.setBrightness(terangMalam);
     }
 
-    Blynk.virtualWrite(V3, "Ada Mobil");
+    if (ultra1Val < batasBunyi) {
+      // Semakin dekat jaraknya, semakin cepat beep
+      int beepDelay = map(ultra1Val, 0, batasBunyi, 50, 500);  // Map jarak ke delay antara beep
+      digitalWrite(buzzerPin, HIGH);
+      delay(beepDelay);
+      digitalWrite(buzzerPin, LOW);
+      delay(beepDelay);
+    } else {
+      // Jika jarak lebih dari 10 cm, matikan buzzer
+      digitalWrite(buzzerPin, LOW);
+    }
+
+    if (modeOffline == false) {
+      Blynk.virtualWrite(V3, "Ada Mobil");
+    }
 
   } else {
     if (nowSiang) {
@@ -110,7 +170,9 @@ void myTimerEvent() {
     } else {
       dimmer1.setBrightness(redupMalam);
     }
-    Blynk.virtualWrite(V3, "Kosong");
+    if (modeOffline == false) {
+      Blynk.virtualWrite(V3, "Kosong");
+    }
   }
 
   if (ultra2Val < batasJarak) {
@@ -119,26 +181,46 @@ void myTimerEvent() {
     } else {
       dimmer2.setBrightness(terangMalam);
     }
-    Blynk.virtualWrite(V4, "Ada Mobil");
 
+    if (ultra2Val < batasBunyi) {
+      // Semakin dekat jaraknya, semakin cepat beep
+      int beepDelay2 = map(ultra2Val, 0, batasBunyi, 50, 500);  // Map jarak ke delay antara beep
+      digitalWrite(buzzerPin, HIGH);
+      delay(beepDelay2);
+      digitalWrite(buzzerPin, LOW);
+      delay(beepDelay2);
+    } else {
+      // Jika jarak lebih dari 10 cm, matikan buzzer
+      digitalWrite(buzzerPin, LOW);
+    }
+    if (modeOffline == false) {
+      Blynk.virtualWrite(V4, "Ada Mobil");
+    }
   } else {
     if (nowSiang) {
       dimmer2.setBrightness(redupSiang);
     } else {
       dimmer2.setBrightness(redupMalam);
     }
-    Blynk.virtualWrite(V4, "Kosong");
+    if (modeOffline == false) {
+      Blynk.virtualWrite(V4, "Kosong");
+    }
   }
 
   if ((infra1Val != lastInfra1) || (infra2Val != lastInfra2)) {
     if (infra1Val == 1 || infra2Val == 1) {
-      Blynk.virtualWrite(V0, 1);
+      if (modeOffline == false) {
+        Blynk.virtualWrite(V0, 1);
+      }
       myservo.write(servoPin, sudutBuka);
       Serial.println("BUKA PORTAL");
     }
 
     if (infra1Val == 0 && infra2Val == 0) {
-      Blynk.virtualWrite(V0, 0);
+      if (modeOffline == false) {
+        Blynk.virtualWrite(V0, 0);
+      }
+
       myservo.write(servoPin, sudutTutup);
       Serial.println("TUTUP PORTAL");
     }
@@ -164,7 +246,10 @@ BLYNK_WRITE(V0) {
 }
 
 void loop() {
-  Blynk.run();
+  if (modeOffline == false) {
+    Blynk.run();
+  }
+
   timer.run();
 }
 
@@ -180,4 +265,10 @@ void debugSensor() {
   Serial.print("\tultra2Val: ");
   Serial.print(ultra2Val);
   Serial.println();
+}
+
+void kedipLed(float detik) {
+  digitalWrite(ledPin, HIGH);
+  delay(detik * 1000);
+  digitalWrite(ledPin, LOW);
 }
