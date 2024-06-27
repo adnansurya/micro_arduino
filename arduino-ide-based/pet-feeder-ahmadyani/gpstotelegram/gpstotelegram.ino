fd63c992703ede08944cdab2f3abb732323f5586
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-06-27 08:48:31 +0700

    Update gpstotelegram.ino

--- arduino-ide-based/pet-feeder-ahmadyani/gpstotelegram/gpstotelegram.ino
+++ arduino-ide-based/pet-feeder-ahmadyani/gpstotelegram/gpstotelegram.ino
@@ -8,13 +8,17 @@
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
+#define gpsRx 5    //(D1)
+#define gpsTx 4    //(D2)
+#define buzzer 15  //(D8)
+
 TinyGPSPlus gps;
-SoftwareSerial SerialGPS(4, 5);
+SoftwareSerial SerialGPS(gpsTx, gpsRx);  //D2 to Tx, D1 to Rx
 
-const char* ssid = "LAGILAGI";
-const char* password = "rotibakar";
+const char* ssid = "MIKRO";
+const char* password = "IDEAlist";
 
-#define BOT_TOKEN "1389983359:AAHGPAMEwdwmO6Bd_PgtU1cvFPNC65r-oXM"
+#define BOT_TOKEN "1115765927:AAFW2T18s1AcQf7SGgrVChC6mSjhN4YsAgo"
 #define CHAT_ID "108488036"
 
 const unsigned long BOT_MTBS = 3000;
@@ -28,14 +32,24 @@ float Latitude, Longitude;
 int year, month, date, hour, minute, second;
 String DateString, TimeString, LatitudeString, LongitudeString;
 
+bool locationOk = false;
+bool spamGPS = false;
 
-ESP8266WebServer server(80);
+unsigned long durasiSpam = 60000;
+unsigned long lastSpam;
+
+
 void setup() {
+  pinMode(buzzer, OUTPUT);
+
+  beep(1, 0.5);
+
   Serial.begin(115200);
   SerialGPS.begin(9600);
   Serial.println();
   Serial.print("Connecting");
   WiFi.begin(ssid, password);
+
   secured_client.setTrustAnchors(&cert);  // Add root certificate for api.telegram.org
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -54,21 +68,41 @@ void setup() {
   }
   Serial.println(now);
 
-
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", handleRoot);
-  server.begin();
   Serial.println("HTTP server started");
   Serial.println(WiFi.localIP());
 
-  bot.sendMessage(CHAT_ID, "IP Local : " + WiFi.localIP().toString(), "");
+  bot.sendMessage(CHAT_ID, "GPS Terhubung!", "");
+  beep(2, 0.2);
+}
+
+void beep(int ulang, float detik) {
+  for (int i = 0; i < ulang; i++) {
+    digitalWrite(buzzer, HIGH);
+    delay(detik * 1000);
+    digitalWrite(buzzer, LOW);
+    delay(detik * 1000);
+  }
 }
 
 
 void loop() {
+
+  if (gps.location.isValid()) {
+    
+  }
+
+  if (locationOk && spamGPS && millis() > (lastSpam + durasiSpam)) {
+    bot.sendMessage(CHAT_ID, htmlBuilder(LatitudeString, LongitudeString), "HTML");
+    lastSpam = millis();
+  }
+
+  if (LatitudeString != "" && locationOk == false) {
+    beep(3, 0.4);
+    locationOk = true;
+    bot.sendMessage(CHAT_ID, htmlBuilder(LatitudeString, LongitudeString), "HTML");
+  }
+
+
 
   while (SerialGPS.available() > 0) {
     if (gps.encode(SerialGPS.read())) {
@@ -125,74 +159,30 @@ void loop() {
   }
 
   if (millis() > bot_lasttime + BOT_MTBS) {
-    Serial.println("scan telegram");
+    // Serial.println("scan telegram");
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
     while (numNewMessages) {
+      beep(1, 0.1);
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
 
     bot_lasttime = millis();
-
-  } else {
-
-    server.handleClient();
-    MDNS.update();
   }
 }
 
-const int led = 13;
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/html", htmlBuilder(true));
-  digitalWrite(led, 0);
-}
 
-String htmlBuilder(bool forWeb) {
+String htmlBuilder(String lat, String lng) {
   String s = "";
-  if (forWeb) {
-    s += "<!DOCTYPE html> <html> <head> <title>NEO-6M GPS Readings</title> <style>";
-    s += "table, th, td {border: 1px solid blue;} </style> </head> <body> <h1  style=";
-    s += "font-size:300%;";
-    s += " ALIGN=CENTER>NEO-6M GPS Readings</h1>";
-    s += "<p ALIGN=CENTER style="
-         "font-size:150%;"
-         "";
-    s += "> <b>Location Details</b></p> <table ALIGN=CENTER style=";
-    s += "width:50%";
-    s += "> <tr> <th>Latitude</th>";
-    s += "<td ALIGN=CENTER >";
-    s += LatitudeString;
-    s += "</td> </tr> <tr> <th>Longitude</th> <td ALIGN=CENTER >";
-    s += LongitudeString;
-    s += "</td> </tr> <tr>  <th>Date</th> <td ALIGN=CENTER >";
-    s += DateString;
-    s += "</td></tr> <tr> <th>Time</th> <td ALIGN=CENTER >";
-    s += TimeString;
-    s += "</td>  </tr> </table> ";
-  }
-
-  if (gps.location.isValid()) {
-    s += "<p align=center><a style="
-         "color:RED;font-size:125%;"
-         " href="
-         "http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=";
-    s += LatitudeString;
-    s += "+";
-    s += LongitudeString;
-    s += ""
-         " target="
-         "_top"
-         ">Click here</a> to open the location in Google Maps.</p>";
-  }
-
-  if (forWeb) {
-    s += "</body> </html> \n";
-  }
-
+  s += "<b><a href="
+       "\"http://maps.google.com/maps?&z=7&mrt=yp&t=k&q=";
+  s += lat;
+  s += "+";
+  s += lng;
+  s += "\">Klik Untuk Melihat Lokasi</a></b>";
   return s;
 }
 
@@ -205,11 +195,37 @@ void handleNewMessages(int numNewMessages) {
     }
 
     String perintah = bot.messages[i].text;
+    perintah.trim();
     Serial.println(perintah);
-    if (perintah == "/gps") {
-      bot.sendMessage(CHAT_ID, htmlBuilder(false), "");
+    if (perintah == "/update") {
+      if (locationOk) {
+        bot.sendMessage(CHAT_ID, "Update Terakhir : " + DateString + ", " + TimeString, "HTML");
+        bot.sendMessage(CHAT_ID, htmlBuilder(LatitudeString, LongitudeString), "HTML");        
+      } else {
+        bot.sendMessage(CHAT_ID, "<b>Lokasi Belum Tersedia</b>", "HTML");
+      }
+      beep(1, 0.2);
+    } else if (perintah == "/dummy") {
+      bot.sendMessage(CHAT_ID, htmlBuilder("0.0", "0.0"), "HTML");
+      beep(1, 0.2);
+    } else if (perintah.indexOf("/spam") > -1) {
+      durasiSpam = (long)perintah.substring(6).toInt();
+
+      if (durasiSpam == 0) {
+        durasiSpam = 1;
+      }
+
+      bot.sendMessage(CHAT_ID, "Lokasi akan dikirimkan setiap " + String(durasiSpam) + " menit", "HTML");
+      beep(1, 0.2);
+      durasiSpam = durasiSpam * 60000;
+      spamGPS = true;
+    } else if (perintah == "/mute") {
+      spamGPS = false;
+      bot.sendMessage(CHAT_ID, "Pengiriman lokasi berkala dimatikan", "HTML");
+      beep(1, 0.2);
     } else {
       bot.sendMessage(CHAT_ID, "Perintah tak dikenali", "");
+      beep(1, 1);
     }
   }
 }
