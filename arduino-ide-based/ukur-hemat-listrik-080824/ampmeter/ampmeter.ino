c43d8c821bb264881e3c8ea31624e2e21b97584a
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-29 20:54:31 +0800

    amperemeter 3 channel

--- arduino-ide-based/ukur-hemat-listrik-080824/ampmeter/ampmeter.ino
+++ arduino-ide-based/ukur-hemat-listrik-080824/ampmeter/ampmeter.ino
@@ -8,6 +8,8 @@ ACS712 sensor1(ACS712_05B, sensor1Pin);
 ACS712 sensor2(ACS712_05B, sensor2Pin);
 ACS712 sensor3(ACS712_05B, sensor3Pin);
 
+float current1, current2, current3;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("START");
@@ -15,8 +17,25 @@ void setup() {
   sensor1.calibrate();
   sensor2.calibrate();
   sensor3.calibrate();
-  Serial.println("Calibrating Sensor OK");  
+  Serial.println("Calibrating Sensor OK");
 }
 
 void loop() {
+  current1 = sensor1.getCurrentAC();
+  current2 = sensor2.getCurrentAC();
+  current3 = sensor3.getCurrentAC();
+
+  // Send it to serial
+  Serial.print("I_1 = ");
+  Serial.print(current1);
+  Serial.print(" A\t");
+  Serial.print("I_2 = ");
+  Serial.print(current2);
+  Serial.print(" A\t");
+  Serial.print("I_3 = ");
+  Serial.print(current3);
+  Serial.println(" A");
+
+  // Wait one second before the new cycle
+  delay(1000);
 }
