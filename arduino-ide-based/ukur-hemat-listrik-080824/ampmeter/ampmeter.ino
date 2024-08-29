f8ccb0a65dc5a191db763f14bd38739326b4fcca
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2024-08-29 20:59:22 +0800

    add 4th channel

--- arduino-ide-based/ukur-hemat-listrik-080824/ampmeter/ampmeter.ino
+++ arduino-ide-based/ukur-hemat-listrik-080824/ampmeter/ampmeter.ino
@@ -1,14 +1,16 @@
 #include "ACS712.h"
 
-#define sensor1Pin A1
-#define sensor2Pin A2
-#define sensor3Pin A3
+#define sensor1Pin A0
+#define sensor2Pin A1
+#define sensor3Pin A2
+#define sensor4Pin A3
 
 ACS712 sensor1(ACS712_05B, sensor1Pin);
 ACS712 sensor2(ACS712_05B, sensor2Pin);
 ACS712 sensor3(ACS712_05B, sensor3Pin);
+ACS712 sensor4(ACS712_05B, sensor4Pin);
 
-float current1, current2, current3;
+float current1, current2, current3, current4;
 
 void setup() {
   Serial.begin(9600);
@@ -17,6 +19,7 @@ void setup() {
   sensor1.calibrate();
   sensor2.calibrate();
   sensor3.calibrate();
+  sensor4.calibrate();
   Serial.println("Calibrating Sensor OK");
 }
 
@@ -24,6 +27,7 @@ void loop() {
   current1 = sensor1.getCurrentAC();
   current2 = sensor2.getCurrentAC();
   current3 = sensor3.getCurrentAC();
+  current4 = sensor4.getCurrentAC();
 
   // Send it to serial
   Serial.print("I_1 = ");
@@ -34,6 +38,9 @@ void loop() {
   Serial.print(" A\t");
   Serial.print("I_3 = ");
   Serial.print(current3);
+  Serial.print(" A\t");
+  Serial.print("I_4 = ");
+  Serial.print(current4);
   Serial.println(" A");
 
   // Wait one second before the new cycle
