08a302702264c0311fa600a07b2897ca698e0374
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-01-05 06:06:01 +0800

    progress lansia

--- arduino-ide-based/blynk-kesehatan-lansia/parameter-kesehatan-lansia/parameter-kesehatan-lansia.ino
+++ arduino-ide-based/blynk-kesehatan-lansia/parameter-kesehatan-lansia/parameter-kesehatan-lansia.ino
@@ -1,31 +1,84 @@
+#define BLYNK_TEMPLATE_ID "TMPL6-B_pH2Sg"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "SdgAnvD4LHTiYM6iePQAE_d-qTORZvlU"
+
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
-#include <Adafruit_MAX30100.h>
-#include <MPU6050.h>
+#include <MAX30100_PulseOximeter.h>  // Library Oxullo untuk MAX30100
+#include <Adafruit_MPU6050.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
 
 // Inisialisasi sensor
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-Adafruit_MAX30100 max30100;
-MPU6050 mpu;
+PulseOximeter pox;     // Inisialisasi MAX30100 dari library Oxullo
+Adafruit_MPU6050 mpu;  // Inisialisasi MPU6050 dari library Adafruit
 
 // Variabel untuk data sensor
-float suhuTubuh;
+double suhuTubuh;
 float heartRate, spO2;
-int16_t ax, ay, az, gx, gy, gz;
+float accelX, accelY, accelZ;  // Data akselerasi
+float gyroX, gyroY, gyroZ;     // Data gyroscope
 
 // Variabel untuk teks kondisi
 String kondisi = "";
 
 // Blynk Auth Token
-char auth[] = "Your_Blynk_Auth_Token";  // Ganti dengan Auth Token Anda
-char ssid[] = "Your_WiFi_SSID";         // Ganti dengan nama WiFi Anda
-char pass[] = "Your_WiFi_Password";     // Ganti dengan password WiFi Anda
+char ssid[] = "MIKRO";     // Ganti dengan nama WiFi Anda
+char pass[] = "1DEAlist";  // Ganti dengan password WiFi Anda
+
+BlynkTimer timer;
+
+// Callback untuk pembaruan data dari MAX30100
+void onPulseOximeterData() {
+  Serial.println("Beat!");
+}
+
+void updateData() {
+  // Baca data suhu tubuh dari MLX90614
+  suhuTubuh = mlx.readObjectTempC();
+  Serial.println("Suhu Tubuh: " + String(suhuTubuh) + " °C");
+
+  // Update data dari MAX30100
+  pox.update();
+
+  heartRate = pox.getHeartRate();
+  spO2 = pox.getSpO2();
+
+  Serial.println("Denyut Jantung: " + String(heartRate) + " bpm");
+  Serial.println("Saturasi Oksigen: " + String(spO2) + " %");
+
+  // Baca data akselerasi dan gyroscope dari MPU6050
+  sensors_event_t accel, gyro, temp;
+  mpu.getEvent(&accel, &gyro, &temp);
+  accelX = accel.acceleration.x;
+  accelY = accel.acceleration.y;
+  accelZ = accel.acceleration.z;
+  gyroX = gyro.gyro.x;
+  gyroY = gyro.gyro.y;
+  gyroZ = gyro.gyro.z;
+
+
+
+  Serial.println("Akselerasi: ax=" + String(accelX) + " m/s², ay=" + String(accelY) + " m/s², az=" + String(accelZ) + " m/s²");
+
+  // Kirim data ke Blynk Virtual Pins dengan satuan
+  Blynk.virtualWrite(V1, String(suhuTubuh) + " °C");                                                    // Virtual Pin V1 untuk Suhu Tubuh
+  Blynk.virtualWrite(V2, String(heartRate) + " bpm");                                                   // Virtual Pin V2 untuk Denyut Jantung
+  Blynk.virtualWrite(V3, String(spO2) + " %");                                                          // Virtual Pin V3 untuk Saturasi Oksigen
+  Blynk.virtualWrite(V4, String(sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ)) + " m/s²");  // Virtual Pin V4 untuk Magnitudo Gerakan
+
+  // Periksa dan kirim teks kondisi ke Blynk (V0)
+  checkHealthConditions();
+}
 
 // Fungsi untuk memeriksa kondisi kesehatan
 void checkHealthConditions() {
-  kondisi = ""; // Reset string kondisi
+  kondisi = "";  // Reset string kondisi
 
   // MAX30100: Denyut Jantung (HR)
   if (heartRate < 60) {
@@ -47,8 +100,8 @@ void checkHealthConditions() {
   }
 
   // MPU6050: Gerakan
-  float magnitude = sqrt(ax * ax + ay * ay + az * az) / 16384.0; // Normalisasi akselerasi
-  if (magnitude > 2.5) { // Threshold untuk mendeteksi gerakan mendadak
+  float magnitude = sqrt(accelX * accelX + accelY * accelY + accelZ * accelZ);  // Magnitudo akselerasi
+  if (magnitude > 2.5) {                                                        // Threshold untuk mendeteksi gerakan mendadak
     kondisi += "Gerakan mendadak (kemungkinan jatuh); ";
   }
 
@@ -58,65 +111,45 @@ void checkHealthConditions() {
   }
 
   // Kirim teks kondisi ke Blynk
-  Blynk.virtualWrite(V5, kondisi);
+  Blynk.virtualWrite(V0, kondisi);
 }
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
+
 
   // Inisialisasi WiFi dan Blynk
-  WiFi.begin(ssid, pass);
-  Blynk.begin(auth, ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi Terhubung!");
 
-  // Inisialisasi sensor
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+
+  // Inisialisasi sensor suhu (MLX90614)
   if (!mlx.begin()) {
     Serial.println("MLX90614 gagal diinisialisasi!");
-    while (1);
+    while (1)
+      ;
   }
-  max30100.begin();
-  max30100.setMode(MAX30100_MODE_SPO2_HR);
-  max30100.setLedsPulseAmplitude(0x1F, 0x1F);
-  mpu.initialize();
-  if (!mpu.testConnection()) {
+
+  // Inisialisasi sensor MAX30100
+  if (!pox.begin()) {
+    Serial.println("MAX30100 gagal diinisialisasi!");
+    while (1)
+      ;
+  }
+  pox.setOnBeatDetectedCallback(onPulseOximeterData);
+
+  // Inisialisasi sensor MPU6050
+  if (!mpu.begin()) {
     Serial.println("MPU6050 gagal diinisialisasi!");
-    while (1);
+    while (1)
+      ;
   }
+
+  timer.setInterval(1000L, updateData);
 }
 
 void loop() {
   // Jalankan Blynk
   Blynk.run();
-
-  // Baca data suhu tubuh dari MLX90614
-  suhuTubuh = mlx.readObjectTempC();
-  Serial.println("Suhu Tubuh: " + String(suhuTubuh) + " °C");
-
-  // Baca data denyut jantung dan SpO2 dari MAX30100
-  max30100.update();
-  heartRate = max30100.getHeartRate();
-  spO2 = max30100.getSpO2();
-  Serial.println("Denyut Jantung: " + String(heartRate) + " bpm");
-  Serial.println("Saturasi Oksigen: " + String(spO2) + " %");
-
-  // Baca data akselerometer dari MPU6050
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  Serial.println("Akselerasi: ax=" + String(ax) + ", ay=" + String(ay) + ", az=" + String(az));
-
-  // Kirim data ke Blynk Virtual Pins
-  Blynk.virtualWrite(V1, suhuTubuh);  // Virtual Pin V1 untuk Suhu Tubuh
-  Blynk.virtualWrite(V2, heartRate); // Virtual Pin V2 untuk Denyut Jantung
-  Blynk.virtualWrite(V3, spO2);      // Virtual Pin V3 untuk Saturasi Oksigen
-  Blynk.virtualWrite(V4, sqrt(ax * ax + ay * ay + az * az)); // Virtual Pin V4 untuk Magnitudo Gerakan
-
-  // Periksa dan kirim teks kondisi ke Blynk
-  checkHealthConditions();
-
-  // Tunggu 1 detik sebelum membaca ulang data
-  delay(1000);
+  timer.run();
 }
