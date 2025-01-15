ccacfba8718c3313d1fd918f670f73864464f5f5
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-01-15 23:03:48 +0800

    Update loratx-mq135-lcd.ino

--- arduino-ide-based/blynk-monitoring-apar/loratx-mq135-lcd/loratx-mq135-lcd.ino
+++ arduino-ide-based/blynk-monitoring-apar/loratx-mq135-lcd/loratx-mq135-lcd.ino
@@ -1,6 +1,15 @@
+#define BLYNK_TEMPLATE_ID "TMPL6GsHGgMf0"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "c_cy-OkIWySgf4IXc60_Zt0U65naFriQ"
+
 #include <LoRa.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+#define BLYNK_PRINT Serial
+#define OUT_PIN 2
 
 // Konfigurasi pin LoRa
 #define SS 5
@@ -8,21 +17,31 @@
 #define DIO0 26
 
 // Konfigurasi pin MQ135
-const int analogPinMQ135 = 34; // Pin analog untuk MQ135
-const float RL = 10.0;    // Resistor load dalam kilo-ohm
-const float VCC = 3.3;    // Tegangan catu daya ESP32
-const float ratioCleanAir = 3.62; // Rasio RS/R0 di udara bersih
+const int analogPinMQ135 = 34;     // Pin analog untuk MQ135
+const float RL = 10.0;             // Resistor load dalam kilo-ohm
+const float VCC = 3.3;             // Tegangan catu daya ESP32
+const float ratioCleanAir = 3.62;  // Rasio RS/R0 di udara bersih
 
-float R0 = 1.0; // Nilai referensi R0 (lakukan kalibrasi sebelumnya)
+float R0 = 1.0;  // Nilai referensi R0 (lakukan kalibrasi sebelumnya)
 
 // Konfigurasi sensor tegangan
-const int analogPinVoltage = 35; // Pin analog untuk sensor tegangan
-const float voltageDividerRatio = 5.0; // Rasio pembagi tegangan (sesuaikan dengan sensor Anda)
+const int analogPinVoltage = 35;        // Pin analog untuk sensor tegangan
+const float voltageDividerRatio = 5.0;  // Rasio pembagi tegangan (sesuaikan dengan sensor Anda)
 
 // Konfigurasi LCD I2C
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Alamat I2C LCD biasanya 0x27
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Alamat I2C LCD biasanya 0x27
+
+BlynkTimer mainTimer;
+char ssid[] = "Anafora";   // Ganti dengan nama WiFi Anda
+char pass[] = "12345678";  // Ganti dengan password WiFi Anda
+
+float batt_percent;
+float ppm;
 
 void setup() {
+  pinMode(OUT_PIN, OUTPUT);
+  blinkOut(1);
+
   Serial.begin(115200);
   while (!Serial);
 
@@ -35,7 +54,7 @@ void setup() {
   // Inisialisasi LoRa
   Serial.println("Menginisialisasi LoRa...");
   LoRa.setPins(SS, RST, DIO0);
-  if (!LoRa.begin(915E6)) { // Frekuensi default 915 MHz
+  if (!LoRa.begin(915E6)) {  // Frekuensi default 915 MHz
     Serial.println("Gagal menginisialisasi LoRa!");
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -57,15 +76,27 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("R0: ");
   lcd.setCursor(5, 0);
-  lcd.print(R0, 2); // Tampilkan nilai R0 dengan 2 desimal
-  delay(2000);
+  lcd.print(R0, 2);  // Tampilkan nilai R0 dengan 2 desimal
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Serial.println("Semua sensor berhasil diinisialisasi.");
+
+  mainTimer.setInterval(1000L, mainEvent);
+  mainTimer.setInterval(10000L, sendData);
+
+  blinkOut(2);
 }
 
 void loop() {
+  Blynk.run();
+  mainTimer.run();
+}
+
+void mainEvent() {
   // Membaca data PPM dari MQ135
   float rs = readSensorMQ135();
   float ratio = rs / R0;
-  float ppm = pow(10, ((log10(ratio) - (-0.45)) / (-0.32))); // m dan b dari datasheet
+  ppm = pow(10, ((log10(ratio) - (-0.45)) / (-0.32)));  // m dan b dari datasheet
 
   // Membaca persentase ADC dari MQ135
   int adcValueMQ135 = analogRead(analogPinMQ135);
@@ -73,60 +104,70 @@ void loop() {
 
   // Membaca tegangan dari sensor tegangan
   float voltage = readVoltage();
-
-  // Kirim data melalui LoRa
-  Serial.print("Mengirim PPM CO: ");
-  Serial.print(ppm);
-  Serial.print(" ppm, Tegangan: ");
-  Serial.print(voltage);
-  Serial.println(" V");
-
-  LoRa.beginPacket();
-  LoRa.print("PPM: ");
-  LoRa.print(ppm);
-  LoRa.print(", Tegangan: ");
-  LoRa.print(voltage);
-  LoRa.print(" V");
-  LoRa.endPacket();
+  batt_percent = voltage / 4.2 * 100.0;
 
   // Tampilkan data pada LCD
-  displayOnLCD(ppm, adcPercentageMQ135, voltage);
+  displayOnLCD(ppm, batt_percent);
+}
 
-  delay(2000); // Delay 2 detik sebelum pengukuran berikutnya
+void sendData() {
+  if (Blynk.connected()) {
+    Serial.println("Koneksi internet tersedia. Mengirim data ke Blynk...");
+    Blynk.virtualWrite(V0, ppm);
+    Blynk.virtualWrite(V1, batt_percent);
+  } else {
+    Serial.println("Koneksi internet tidak tersedia. Mengirim data melalui LoRa...");
+    LoRa.beginPacket();
+    LoRa.print("PPM: ");
+    LoRa.print(ppm);
+    LoRa.print(", Tegangan: ");
+    LoRa.print(batt_percent);
+    LoRa.print("%");
+    LoRa.endPacket();
+  }
 }
 
 float readSensorMQ135() {
   int adcValue = analogRead(analogPinMQ135);
-  float sensorVoltage = (adcValue / 4095.0) * VCC; // Konversi ke tegangan (12-bit ADC)
-  float rs = ((VCC - sensorVoltage) / sensorVoltage) * RL; // Hitung RS
+  float sensorVoltage = (adcValue / 4095.0) * VCC;          // Konversi ke tegangan (12-bit ADC)
+  float rs = ((VCC - sensorVoltage) / sensorVoltage) * RL;  // Hitung RS
   return rs;
 }
 
 float calibrateSensor() {
   float totalRS = 0;
-  for (int i = 0; i < 100; i++) { // Ambil rata-rata dari 100 pembacaan
+  for (int i = 0; i < 100; i++) {  // Ambil rata-rata dari 100 pembacaan
     totalRS += readSensorMQ135();
     delay(50);
   }
   float avgRS = totalRS / 100.0;
-  return avgRS / ratioCleanAir; // Hitung R0 berdasarkan rasio udara bersih
+  return avgRS / ratioCleanAir;  // Hitung R0 berdasarkan rasio udara bersih
 }
 
 float readVoltage() {
-  int adcValue = analogRead(analogPinVoltage);        // Baca nilai analog
-  float sensorVoltage = (adcValue / 4095.0) * VCC;    // Tegangan pada pin ADC
-  return sensorVoltage * voltageDividerRatio;         // Hitung tegangan aktual
+  int adcValue = analogRead(analogPinVoltage);      // Baca nilai analog
+  float sensorVoltage = (adcValue / 4095.0) * VCC;  // Tegangan pada pin ADC
+  return sensorVoltage * voltageDividerRatio;       // Hitung tegangan aktual
 }
 
-void displayOnLCD(float ppm, float adcPercentage, float voltage) {
+void displayOnLCD(float ppm, float percentage) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("PPM: ");
-  lcd.print(ppm, 2); // Tampilkan nilai PPM dengan 2 desimal
+  lcd.print("CO : ");
+  lcd.print(ppm, 2);  // Tampilkan nilai PPM dengan 2 desimal
+  lcd.print(" ppm");
 
   lcd.setCursor(0, 1);
-  lcd.print("V: ");
-  lcd.print(voltage, 2); // Tampilkan tegangan dengan 2 desimal
-  lcd.print(" ADC: ");
-  lcd.print(adcPercentage, 1); // Tampilkan persentase ADC dengan 1 desimal
+  lcd.print("Bat: ");
+  lcd.print(percentage, 2);  // Tampilkan tegangan dengan 2 desimal
+  lcd.print(" %");
+}
+
+void blinkOut(int rep) {
+  for (int i = 0; i < rep; i++) {
+    digitalWrite(OUT_PIN, HIGH);
+    delay(200);
+    digitalWrite(OUT_PIN, LOW);
+    delay(200);
+  }
 }
