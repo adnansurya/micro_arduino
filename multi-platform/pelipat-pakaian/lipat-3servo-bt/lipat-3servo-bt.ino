e7e88d20c4bc788f0a67bde47f0eeace11fdd749
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-02-07 12:23:54 +0800

    Create lipat-3servo-bt.ino

---
+++ multi-platform/pelipat-pakaian/lipat-3servo-bt/lipat-3servo-bt.ino
@@ -0,0 +1,62 @@
+#include <Servo.h>
+
+#define PIN_KANAN 9
+#define PIN_ATAS 10
+#define PIN_KIRI 11
+
+#define idleKanan 120
+#define idleAtas 0
+#define idleKiri 0
+
+
+#define lipatKanan 0
+#define lipatAtas 180
+#define lipatKiri 120
+
+Servo servoKanan, servoAtas, servoKiri;  // create Servo object to control a servo
+
+
+void setup() {
+  // put your setup code here, to run once:
+  servoKanan.attach(PIN_KANAN);
+  servoKiri.attach(PIN_KIRI);
+  servoAtas.attach(PIN_ATAS);
+
+  servoKanan.write(idleKanan);
+  servoAtas.write(idleAtas);
+  servoKiri.write(idleKiri);
+
+  Serial.begin(9600);
+  Serial.println("Ready");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String perintah = Serial.readStringUntil('\n');
+    perintah.trim();
+    if (perintah == "lipat") {
+      lipat(2500);
+    }
+  }
+  delay(10);
+}
+
+
+
+void lipat(int delayLipat) {
+  Serial.println("Mulai");
+  servoKanan.write(lipatKanan);
+  delay(delayLipat);
+  servoKanan.write(idleKanan);
+  delay(delayLipat);
+  servoKiri.write(lipatKiri);
+  delay(delayLipat);
+  servoKiri.write(idleKiri);
+  delay(delayLipat);
+  servoAtas.write(lipatAtas);
+  delay(delayLipat);
+  servoAtas.write(idleAtas);
+  delay(delayLipat);
+
+  Serial.println("Selesai");
+}
