8078307a243ff458da517b7b1180c353a2e7c641
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-03-11 14:11:16 +0800

    restore wifi manager and adding beel function

--- arduino-ide-based/wireless_calling_system/rssi_udp/rssi_udp.ino
+++ arduino-ide-based/wireless_calling_system/rssi_udp/rssi_udp.ino
@@ -1,13 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <WiFiManager.h>
+
+#define BUZZER_PIN D5
 
 // Parameter daya sinyal dan faktor pengurangan
 const int signalAtOneMeter = -40; // Nilai RSSI pada jarak 1 meter (dBm)
 const float pathLossExponent = 3.0; // Faktor pengurangan sinyal (n)
 
-// Konfigurasi WiFi
-const char *ssid = "SSID_WIFI";        // Ganti dengan SSID jaringan WiFi Anda
-const char *password = "PASSWORD_WIFI"; // Ganti dengan password WiFi Anda
+const char *ssid_default = "Wireless Caller";
+
 
 // Konfigurasi UDP
 WiFiUDP udp;
@@ -15,14 +17,23 @@ const char *udpAddress = "192.168.1.101"; // Ganti dengan alamat IP perangkat pe
 const int udpPort = 12345; // Port tujuan
 
 void setup() {
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  beep(BUZZER_PIN, 1, 100);
   Serial.begin(115200);
 
-  // Menyambungkan ke WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+   // Membuat instance WiFiManager
+  WiFiManager wifiManager;
+
+  // Memulai WiFiManager (apabila belum disetel, akan masuk ke mode konfigurasi)
+  if (!wifiManager.autoConnect(ssid_default)) {
+    Serial.println("Gagal menghubungkan ke WiFi, rebooting...");
+    delay(3000);
+    ESP.restart();
   }
+
+  beep(BUZZER_PIN, 2, 200);
+  
   Serial.println("\nTerhubung ke WiFi");
   Serial.print("Alamat IP Pengirim: ");
   Serial.println(WiFi.localIP());
@@ -43,7 +54,7 @@ void loop() {
   Serial.println(distance);
 
   // Menyiapkan pesan untuk dikirimkan
-  String message = "Device1;RSSI:" + String(rssi) + ";Distance:" + String(distance);
+  String message = "Device2;RSSI:" + String(rssi) + ";Distance:" + String(distance);
 
   // Mengirim pesan melalui UDP
   udp.beginPacket(udpAddress, udpPort);
@@ -53,4 +64,14 @@ void loop() {
   Serial.println("Data dikirim melalui UDP!");
 
   delay(1000); // Mengirimkan data setiap detik
+}
+
+
+void beep(int outpin, int freq, int delayms) {
+  for (int i = 0; i < freq; i++) {
+    digitalWrite(outpin, HIGH);
+    delay(delayms);
+    digitalWrite(outpin, LOW);
+    delay(delayms);
+  }
 }
