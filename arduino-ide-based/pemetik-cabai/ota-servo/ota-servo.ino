086fb6d5eefc967489f5893d710c006220da8157
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-06-27 10:04:57 +0800

    redesign web

--- arduino-ide-based/pemetik-cabai/ota-servo/ota-servo.ino
+++ arduino-ide-based/pemetik-cabai/ota-servo/ota-servo.ino
@@ -30,28 +30,39 @@ const char index_html[] PROGMEM = R"rawliteral(
 <body>
   <h2>Kendali Lengan Robot OTA</h2>
   <div>
-    <label>Servo 1: <span id="s1val">90</span>°</label><br>
+    <label>Atas - (<span id="s1val">90</span>&#176;) - Bawah</label><br>
     <input type="range" min="0" max="180" value="90" id="s1" oninput="updateServo(this.value,1)">
   </div><br>
   <div>
-    <label>Servo 2: <span id="s2val">90</span>°</label><br>
+    <label>Kiri - (<span id="s2val">90</span>&#176;) - Kanan</label><br>
     <input type="range" min="0" max="180" value="90" id="s2" oninput="updateServo(this.value,2)">
   </div><br>
   <div>
-    <label>Servo 3: <span id="s3val">90</span>°</label><br>
-    <input type="range" min="60" max="180" value="120" id="s3" oninput="updateServo(this.value,3)">
+    <label>Mundur - (<span id="s3val">110</span>&#176;) - Maju</label><br>
+    <input type="range" min="50" max="170" value="110" id="s3" oninput="updateServo(this.value,3)">
   </div><br>
   <div>
-    <label>Servo 4: <span id="s4val">50</span>°</label><br>
-    <input type="range" min="25" max="75" value="50" id="s4" oninput="updateServo(this.value,4)">
-  </div>
+    <label>Lepas - (<span id="s4val">50</span>&#176;) - Jepit</label><br>
+    <input type="range" min="30" max="70" value="50" id="s4" oninput="updateServo(this.value,4)">
+  </div><br>
+  <button onclick="jepit()">JEPIT OTOMATIS</button>
   <script>
     function updateServo(val, id) {
       document.getElementById("s" + id + "val").innerText = val;
       fetch(`/servo?id=${id}&val=${val}`);
     }
+
+    function jepit() {
+      fetch("/jepit");
+    }
   </script>
-</body>
+    <script>
+      function updateServo(val, id) {
+        document.getElementById("s" + id + "val").innerText = val;
+        fetch(`/servo?id=${id}&val=${val}`);
+      }
+    </script>
+  </body>
 </html>
 )rawliteral";
 
@@ -72,12 +83,12 @@ void setup() {
   // Posisi awal
   servo1.write(90);
   servo2.write(90);
-  servo3.write(90);
+  servo3.write(110);
   servo4.write(50);
 
   // Jalankan sebagai Access Point
-  const char* ssid = "LenganRobotESP32";
-  const char* password = "12345678";
+  const char *ssid = "LenganRobotESP32";
+  const char *password = "12345678";
   WiFi.softAP(ssid, password);
 
   IPAddress IP = WiFi.softAPIP();
@@ -85,20 +96,20 @@ void setup() {
   Serial.println(IP);
 
   // Routing utama
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/html", index_html);
   });
 
   // Kendali servo via GET parameter
-  server.on("/servo", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/servo", HTTP_GET, [](AsyncWebServerRequest *request) {
     if (request->hasParam("id") && request->hasParam("val")) {
       int id = request->getParam("id")->value().toInt();
       int val = request->getParam("val")->value().toInt();
       val = constrain(val, 0, 180);
 
-      switch(id) {
+      switch (id) {
         case 1: servo1.write(val); break;
-        case 2: servo2.write(val); break;
+        case 2: servo2.write(180 - val); break;
         case 3: servo3.write(val); break;
         case 4: servo4.write(val); break;
       }
@@ -109,25 +120,31 @@ void setup() {
     }
   });
 
-  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", 
-      "<form method='POST' action='/update' enctype='multipart/form-data'>"
-      "<input type='file' name='update'><input type='submit' value='Upload'>"
-      "</form>");
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/html",
+                  "<form method='POST' action='/update' enctype='multipart/form-data'>"
+                  "<input type='file' name='update'><input type='submit' value='Upload'>"
+                  "</form>");
+  });
+
+  server.on("/jepit", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Jepit otomatis dimulai");
+    jepitOtomatis();
   });
 
   // Proses OTA update
-  server.on("/update", HTTP_POST, 
-    [](AsyncWebServerRequest *request){
+  server.on(
+    "/update", HTTP_POST,
+    [](AsyncWebServerRequest *request) {
       bool updateHasError = Update.hasError();
       request->send(200, "text/plain", updateHasError ? "Update Gagal" : "Update Berhasil. ESP akan restart.");
       if (!updateHasError) {
         delay(1000);
         ESP.restart();
       }
-    }, 
-    [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
-      if (!index){
+    },
+    [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+      if (!index) {
         Serial.printf("Upload Start: %s\n", filename.c_str());
         if (!Update.begin(UPDATE_SIZE_UNKNOWN)) Update.printError(Serial);
       }
@@ -143,12 +160,26 @@ void setup() {
           Update.printError(Serial);
         }
       }
-    }
-  );
+    });
 
   server.begin();
 }
 
+
 void loop() {
   // Tidak digunakan
+}
+
+void jepitOtomatis() {
+  const int lepas = 50;
+  const int jepit = 70;
+  const int jeda = 200; // delay per gerakan (ms)
+  const int ulang = 10;  // berapa kali gerakan jepit-lepas
+
+  for (int i = 0; i < ulang; i++) {
+    servo4.write(jepit);
+    delay(jeda);
+    servo4.write(lepas);
+    delay(jeda);
+  }
 }
