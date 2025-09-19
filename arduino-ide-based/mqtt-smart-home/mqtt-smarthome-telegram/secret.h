08638451c6ae49ef5397ccd2e13e3b7cb608a3ed
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-09-19 09:50:48 +0800

    Update secret.h

--- arduino-ide-based/mqtt-smart-home/mqtt-smarthome-telegram/secret.h
+++ arduino-ide-based/mqtt-smart-home/mqtt-smarthome-telegram/secret.h
@@ -2,7 +2,7 @@
 #define SECRET_H
 
 // Konfigurasi Telegram Bot
-#define BOT_TOKEN "YOUR_TELEGRAM_BOT_TOKEN"
-#define CHAT_ID "YOUR_CHAT_ID"
+#define BOT_TOKEN "7810554937:AAF99TwC4yoWJJnVMUfTn3ZZM6E0SxogjXc"
+#define CHAT_ID "8396048554"
 
 #endif
