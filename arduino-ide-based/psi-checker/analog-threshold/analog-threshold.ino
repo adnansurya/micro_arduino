18d94dee7bae1f6e966714d8634799caa836b308
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2026-04-17 02:30:25 +0800

    Create analog-threshold.ino

---
+++ arduino-ide-based/psi-checker/analog-threshold/analog-threshold.ino
@@ -0,0 +1,56 @@
+// --- Konfigurasi Pin ---
+const int pinSensorPSI = A0;   // Pin Signal (In) sensor
+const int pinLedMerah  = 8;
+const int pinLedBiru   = 9;
+const int pinLedHijau  = 10;
+const int pinBuzzer    = 11;
+
+// --- Konfigurasi Batas Tegangan (Volt) ---
+const float voltMinimal = 1.5; // Ubah nilai ini sesuai batas PSI rendah (0.0 - 5.0 Volt)
+
+void setup() {
+  pinMode(pinLedMerah, OUTPUT);
+  pinMode(pinLedBiru, OUTPUT);
+  pinMode(pinLedHijau, OUTPUT);
+  pinMode(pinBuzzer, OUTPUT);
+  
+  Serial.begin(9600);
+
+  // --- Beep Awal & Blue LED ---
+  digitalWrite(pinLedBiru, HIGH);
+  tone(pinBuzzer, 1000, 1000); 
+  delay(1000);
+  digitalWrite(pinLedBiru, LOW);
+}
+
+void loop() {
+  // 1. Baca nilai mentah ADC (0-1023)
+  int adcValue = analogRead(pinSensorPSI);
+  
+  // 2. Konversi nilai ADC ke Voltase menggunakan rumus:
+  // Voltase = (Nilai_ADC / 1023.0) * 5.0
+  float teganganSaatIni = adcValue * (5.0 / 1023.0);
+
+  // 3. Bandingkan dengan Batas Tegangan
+  if (teganganSaatIni < voltMinimal) {
+    // KONDISI TEGANGAN RENDAH (ERROR)
+    digitalWrite(pinLedMerah, HIGH);
+    digitalWrite(pinLedHijau, LOW);
+    
+    // Beep putus-putus
+    tone(pinBuzzer, 800);
+    delay(150);
+    noTone(pinBuzzer);
+    delay(150);
+  } else {
+    // KONDISI NORMAL
+    digitalWrite(pinLedHijau, HIGH);
+    digitalWrite(pinLedMerah, LOW);
+    noTone(pinBuzzer);
+  }
+
+  // Monitor di Serial
+  Serial.print("Tegangan Sensor: ");
+  Serial.print(teganganSaatIni);
+  Serial.println(" V");
+}
