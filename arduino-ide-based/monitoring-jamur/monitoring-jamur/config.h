32bee8fc49e74178e17b632402991df6ae3e62e6
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2026-07-20 14:43:23 +0800

    Create config.h

---
+++ arduino-ide-based/monitoring-jamur/monitoring-jamur/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Konfigurasi Wi-Fi Rumah / Hotspot HP
+const char* SECRET_SSID = "Hana";
+const char* SECRET_PASS = "74757677";
+
+// URL Web App Google Apps Script
+const char* WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw8kSNRQZ2OUrIDOxFw0MmzpUyhmF1Su6o4FODbJw6dLm594EV_KaIMlpnzdkNPG9Z-LQ/exec";
+
+#endif
